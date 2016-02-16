5d5a20fe08e627062ba4a5f2b870ddcba6282f8b
Dino <dinosaur3366@gmail.com>
2016-02-15 22:45:26 -0500

    steam calculator

--- helloworld/hw.js
+++ helloworld/hw.js
@@ -1,6 +1,28 @@
-new Vue({
-  el: '#app',
-  data: {
-    message: 'Hello Vue.js!'
+var demo = new Vue({
+    el: '#app',
+
+    data: {
+      userid: '',
+      steam_id_url: '',
+      owned_game_url: '',
+      apikey: 'FC65FE32E5732FC54BC70256CDA122BB',
+      steam_id: ''
+    },
+
+    methods: {
+      reverseMessage: function() {
+        this.owned_game_url = 'http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=' + this.apikey + '&steamid=' + this.userid + '&format=json';
+        this.steam_id_url = 'http://api.steampowered.com/ISteamUser/ResolveVanityURL/v0001/?key=' + this.apikey + '&vanityurl=' + this.userid;
+
+        $.getJSON(this.steam_id_url, function(result) {
+        	var jsonString = JSON.stringify(result);
+        	var result = JSON.parse(jsonString);
+        	console.log(result); 
+          //this.steam_id = result.response.steamid;
+        });
+      }
+    }
   }
-})
+
+)
+
